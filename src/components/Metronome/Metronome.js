06d2a09171587a200e95385946c53cba1cb21098
David R Poindexter III <drpoindexter@gmail.com>
2017-08-24 15:57:41 -0400

    Add a change handler to controlled input slider

--- src/components/Metronome/Metronome.js
+++ src/components/Metronome/Metronome.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './Metronome.css';
 
 class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    };
+  }
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+    this.setState({ bpm });
+  }
+
   render() {
-    let bpm = 100;
-    let playing = false;
+    const { playing, bpm } = this.state;
 
     return (
       <div className="metronome">
@@ -14,7 +29,8 @@ class Metronome extends Component {
             type="range"
             min="60"
             max="240"
-            value={bpm}/>
+            value={bpm}
+            onChange={this.handleBpmChange} />
         </div>
         <button>
           { playing ? 'Stop' : 'Start' }
